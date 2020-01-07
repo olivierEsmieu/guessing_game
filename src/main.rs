8977fbb53db137da8f49d58b5fce8a10a5cdaf41
olivier <olivier.esmieu@gmail.com>
2020-01-07 19:14:41 +0100

    nouveau commit

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,27 @@ fn main() {
     println!("deviner le nombre");
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("le n° secret est le {}", secret_number);
-    println!("entre un entier");
+    loop {
+        println!("entre un entier");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
-    let guess: u32 = guess.trim().parse().expect("doit être un entier !!");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, //next loop
+        };
 
-    println!("tu as rentré le {}", guess);
+        println!("tu as rentré le {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("trop petit"),
-        Ordering::Greater => println!("trop grand"),
-        Ordering::Equal => println!("Gagné!!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("trop petit"),
+            Ordering::Greater => println!("trop grand"),
+            Ordering::Equal => {
+                println!("Gagné!!!");
+                break; //sort du loop
+            }
+        }
     }
 }
